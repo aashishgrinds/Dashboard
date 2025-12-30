bf9d7b074a0da0a17f875957e787f4ac255131a4
Aashish <aashishchy951@gmail.com>
2025-12-30 15:32:20 +0545

    dashboard-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { ThemeProvider } from './components/ThemeProvider.tsx';
 import { SidebarProvider, SidebarInset } from './components/ui/sidebar.tsx';
-import AppSidebar from './components/AppSidebar.tsx'
-import Header from './components/Header.tsx'
-import Page from './components/Page.tsx'
+import AppSidebar from './components/AppSidebar.tsx';
+import Header from './components/Header.tsx';
+import { Page, PageHeader } from './components/Page.tsx';
+import { DashboardCard } from './components/DashboardCard.tsx';
 
 const App = () => {
   return (
@@ -10,10 +11,19 @@ const App = () => {
       <SidebarProvider open={false}>
         <AppSidebar />
         <SidebarInset>
-          <Header/>
+          <Header />
           <main>
             <Page>
-              </Page>
+              <PageHeader />
+
+              <div className="">
+                <DashboardCard title='Vendor breakdown'
+                description='Keep track of vendors and their security ratings.'
+                buttonText='View full report'>
+
+                </DashboardCard>
+              </div>
+            </Page>
           </main>
         </SidebarInset>
       </SidebarProvider>
